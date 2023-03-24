64a5b08cf0ecd1d040a8f2f1d045deae2eee851e
hevlyo <riquelme.pereira1@gmail.com>
2023-03-23 21:14:16 -0300

    rendering out the map

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 
-io.on("connection", (socket) => {
-    console.log("user connected", socket.id);
-});
+const loadmap = require('./mapLoader');
 
-app.use(express.static("public"));
-httpServer.listen(5000);
+async function main() {
+    const map2D = await loadmap();
+
+    io.on("connection", (socket) => {
+        console.log("user connected", socket.id);
+
+        socket.emit("map", map2D);
+    });
+
+    app.use(express.static("public"));
+
+    httpServer.listen(5000);
+}
+
+main();
