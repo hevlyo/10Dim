1e0a5ea786da19ce82ff940591a936d0595895fa
hevlyo <riquelme.pereira1@gmail.com>
2023-03-23 22:13:27 -0300

    allowing player movement

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 const mapImage = new Image();
 mapImage.src = "/snowy-sheet.png";
 
+const santaImage = new Image();
+santaImage.src = "/santa.png";
+
 const canvasEl = document.getElementById("canvas");
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
@@ -9,6 +12,7 @@ const canvas = canvasEl.getContext("2d");
 const socket = io(`ws://localhost:5000`);
 
 let map = [[]];
+let players = [];
 
 const TILE_SIZE = 16;
 
@@ -19,6 +23,43 @@ socket.on("connect", () => {
 socket.on("map", (loadedMap) => {
     map = loadedMap;
 });
+
+socket.on("players", (serverPlayers) => {
+   players = serverPlayers;
+ });
+ 
+ const inputs = {
+   up: false,
+   down: false,
+   left: false,
+   right: false,
+ };
+ 
+ window.addEventListener("keydown", (e) => {
+   if (e.key === "w") {
+     inputs["up"] = true;
+   } else if (e.key === "s") {
+     inputs["down"] = true;
+   } else if (e.key === "d") {
+     inputs["right"] = true;
+   } else if (e.key === "a") {
+     inputs["left"] = true;
+   }
+   socket.emit("inputs", inputs);
+ });
+ 
+ window.addEventListener("keyup", (e) => {
+   if (e.key === "w") {
+     inputs["up"] = false;
+   } else if (e.key === "s") {
+     inputs["down"] = false;
+   } else if (e.key === "d") {
+     inputs["right"] = false;
+   } else if (e.key === "a") {
+     inputs["left"] = false;
+   }
+   socket.emit("inputs", inputs);
+ });
 
 function loop() {
     canvas.clearRect(0, 0, canvas.width, canvas.height);
@@ -43,6 +84,10 @@ function loop() {
             );
         }
     }
+
+      for (const player of players) {
+      canvas.drawImage(santaImage, player.x, player.y);
+      }
 
     window.requestAnimationFrame(loop);
 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,68 @@ const app = express();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 
-const loadmap = require('./mapLoader');
+const loadMap = require('./mapLoader');
+
+const SPEED = 5;
+const TICK_RATE = 30;
+
+let players = [];
+const inputsMap = {};
+
+function tick() {
+    for (const player of players) {
+        const inputs = inputsMap[player.id];
+        if (inputs.up) {
+            player.y -= SPEED;
+        } else if (inputs.down) {
+            player.y += SPEED; 
+        }
+
+        if (inputs.left) {
+            player.x -= SPEED;
+        } else if (inputs.right) {
+            player.x += SPEED;
+        }
+    }
+
+    io.emit("players", players);
+}
 
 async function main() {
-    const map2D = await loadmap();
+    const map2D = await loadMap();
 
     io.on("connection", (socket) => {
         console.log("user connected", socket.id);
 
+        inputsMap[socket.id] = {
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        };
+
+        players.push({
+            id: socket.id,
+            x: 0,
+            y: 0,
+        });
+        
         socket.emit("map", map2D);
+
+        socket.on('inputs', (inputs) => {
+            inputsMap[socket.id] = inputs;
+        });
+
+        socket.on('disconnect', () => {
+            players = players.filter(player => player.id != socket.id);
+        })
     });
 
     app.use(express.static("public"));
 
     httpServer.listen(5000);
+
+    setInterval(tick, 1000 / TICK_RATE);
 }
 
 main();
