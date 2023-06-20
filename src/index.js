7c371cfba7acb06d0dc7b6e10b65e3c47f442e01
hevlyo <riquelme.pereira1@gmail.com>
2023-06-19 21:58:04 -0300

    Small improves

--- src/index.js
+++ src/index.js
@@ -1,78 +1,75 @@
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const mapLoader = require("./mapLoader");
+
 const app = express();
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const PORT = process.env.PORT || 4000;
-
-const loadMap = require('./mapLoader');
-
 const SPEED = 5;
-const TICK_RATE = 60;
+const TICK_RATE = 1000 / 60; // 60 ticks per second
 
 let players = [];
 const inputsMap = {};
 
 function tick() {
-    for (const player of players) {
-        const inputs = inputsMap[player.id];
-        if (inputs.up) {
-            player.y -= SPEED;
-        } else if (inputs.down) {
-            player.y += SPEED; 
-        }
+  for (const player of players) {
+    const inputs = inputsMap[player.id];
+    player.x += (inputs.right ? SPEED : 0) - (inputs.left ? SPEED : 0);
+    player.y += (inputs.down ? SPEED : 0) - (inputs.up ? SPEED : 0);
+  }
 
-        if (inputs.left) {
-            player.x -= SPEED;
-        } else if (inputs.right) {
-            player.x += SPEED;
-        }
-    }
-
-    io.emit("players", players);
+  io.emit("players", players);
 }
 
 async function main() {
-    const map2D = await loadMap();
+  const map2D = await mapLoader.loadMap();
 
-    io.on("connection", (socket) => {
-        console.log("user connected", socket.id);
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
 
-        inputsMap[socket.id] = {
-            up: false,
-            down: false,
-            left: false,
-            right: false,
-        };
+    inputsMap[socket.id] = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
 
-        players.push({
-            id: socket.id,
-            voiceId: Math.floor(Math.random() * 1000000),
-            x: 800,
-            y: 800,
-        });
-        
-        socket.emit("map", map2D);
-
-        socket.on('inputs', (inputs) => {
-            inputsMap[socket.id] = inputs;
-        });
-
-        socket.on("mute", (isMuted) => {
-            const player = players.find((player) => player.id === socket.id);
-            player.muted = isMuted;
-        })
-        
-        socket.on('disconnect', () => {
-            players = players.filter(player => player.id != socket.id);
-        });
+    players.push({
+      id: socket.id,
+      voiceId: Math.floor(Math.random() * 1000000),
+      x: 800,
+      y: 800,
     });
 
-    app.use(express.static("public"));
+    socket.emit("map", map2D);
 
-    httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    socket.on("inputs", (inputs) => {
+      inputsMap[socket.id] = inputs;
+    });
 
-    setInterval(tick, 1000 / TICK_RATE);
+    socket.on("mute", (isMuted) => {
+      const player = players.find((player) => player.id === socket.id);
+      if (player) {
+        player.muted = isMuted;
+      }
+    });
+
+    socket.on("disconnect", () => {
+      players = players.filter((player) => player.id !== socket.id);
+      delete inputsMap[socket.id];
+    });
+  });
+
+  app.use(express.static("public"));
+
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+  setInterval(tick, TICK_RATE);
 }
 
 main();
--- src/mapLoader.js
+++ src/mapLoader.js
@@ -1,26 +1,25 @@
-const tmx = require('tmx-parser');
+const tmx = require("tmx-parser");
+const util = require("util");
+const parseFile = util.promisify(tmx.parseFile);
 
 async function loadMap() {
-    const map = await new Promise((resolve, reject) => {
-        tmx.parseFile('./src/map.tmx', function (err, loadedMap) {
-            if (err) return reject(err);
-            resolve(loadedMap);
-        });
-    });
+  const map = await parseFile("./src/map.tmx");
+  const layer = map.layers[0];
+  const map2D = [];
 
-    const layer = map.layers[0];
-    const tiles = layer.tiles;
-    const map2D = [];
-    for (let row = 0; row < map.height; row++) {
-        const tileRow = [];
-        for (let col = 0; col < map.width; col++) {
-            const tile = tiles[row * map.height + col];
-            tileRow.push({ id: tile.id, gid: tile.gid });
-        }
-        map2D.push(tileRow);
+  for (let row = 0; row < map.height; row++) {
+    const tileRow = [];
+    for (let col = 0; col < map.width; col++) {
+      const tileIndex = row * map.width + col;
+      const tile = layer.tiles[tileIndex];
+      tileRow.push({ id: tile.id, gid: tile.gid });
     }
+    map2D.push(tileRow);
+  }
 
-    return map2D;
+  return map2D;
 }
 
-module.exports = loadMap;
+module.exports = {
+  loadMap,
+};
